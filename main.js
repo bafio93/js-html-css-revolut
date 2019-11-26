75d3ace5d09e18f7a667604aa1803798df28a3ea
Previtali_Fabio <fabprev93@gmail.com>
2019-11-26 17:26:31 +0100

    Wacky design, but functional!! :D

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
     $(".header-item > i").click(function(){
-        // $(".submenu").addClass("display-none");
         if ($(this).siblings(".submenu").hasClass("display-none")) {
             $(".submenu").addClass("display-none");
             $(this).siblings(".submenu").removeClass("display-none");
         } else {
             $(".submenu").addClass("display-none");
         }
+        // Very wacky solution indeed!! XD
     });
 });
